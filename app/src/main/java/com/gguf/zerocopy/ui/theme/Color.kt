fdc2a27575d81c338d10fd130ccd4b51eedb2e88
adeennour4-dot <adeenn053@gmail.com>
2026-09-12 07:11:27 +0300

    Add color definitions for UI theme

---
+++ app/src/main/java/com/gguf/zerocopy/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.gguf.zerocopy.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SurfaceDark = Color(0xFF0F1115)
+val SurfaceVariantDark = Color(0xFF1A1D24)
+val PrimaryAccent = Color(0xFF6366F1)
+val SecondaryAccent = Color(0xFF10B981)
